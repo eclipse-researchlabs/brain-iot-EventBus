eea40257cc71987fa66a934db3c6c9c8b87e2830
Derek Baum <derek.baum@paremus.com>
2019-09-17 14:36:18 +0100

    add name + description to eventing-eaxmple behaviours

--- eventing-example/behaviour.impl/src/main/java/com/paremus/brain/iot/example/behaviour/impl/ComponentImpl.java
+++ eventing-example/behaviour.impl/src/main/java/com/paremus/brain/iot/example/behaviour/impl/ComponentImpl.java
@@ -4,60 +4,60 @@
  */
 package com.paremus.brain.iot.example.behaviour.impl;
 
+import com.paremus.brain.iot.example.light.api.LightCommand;
+import com.paremus.brain.iot.example.sensor.api.SensorReadingDTO;
+import eu.brain.iot.eventing.annotation.SmartBehaviourDefinition;
+import eu.brain.iot.eventing.api.EventBus;
+import eu.brain.iot.eventing.api.SmartBehaviour;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-
-import com.paremus.brain.iot.example.light.api.LightCommand;
-import com.paremus.brain.iot.example.sensor.api.SensorReadingDTO;
-
-import eu.brain.iot.eventing.annotation.SmartBehaviourDefinition;
-import eu.brain.iot.eventing.api.EventBus;
-import eu.brain.iot.eventing.api.SmartBehaviour;
-
 /**
  * A Smart Behaviour implementing a security light that slowly turns off
  */
 @Component
-@SmartBehaviourDefinition(consumed=SensorReadingDTO.class, filter="(timestamp=*)")
+@SmartBehaviourDefinition(consumed = SensorReadingDTO.class, filter = "(timestamp=*)",
+        author = "Paremus", name = "Example Smart Security Light Behaviour",
+        description = "Implements a security light that slowly turns off.")
 public class ComponentImpl implements SmartBehaviour<SensorReadingDTO>{
 
 	private final ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
-	
+
 	private final AtomicInteger brightness = new AtomicInteger();
-	
+
 	@Reference
 	private EventBus eventBus;
-	
+
 	@Deactivate
 	void stop() {
 		worker.shutdown();
 	}
-	
+
 	@Override
 	public void notify(SensorReadingDTO event) {
-		
+
 		int oldValue = brightness.getAndSet(10);
-		
+
 		if(oldValue == 0) {
 			worker.execute(this::updateBulb);
 		}
 	}
-	
+
 	private void updateBulb() {
 		int value = brightness.getAndAccumulate(-1, (a,b) -> Math.max(0, a + b));
-		
+
 		LightCommand command = new LightCommand();
 		command.brightness = value;
 		command.status = value > 0;
-		
+
 		eventBus.deliver(command);
-		
+
 		if(value != 0) {
 			worker.schedule(this::updateBulb, 1, TimeUnit.SECONDS);
 		}
